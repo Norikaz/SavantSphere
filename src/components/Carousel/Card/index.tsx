a6e9ecede98e006c339edf8ba678b33cd6fd3e79
Norikaz <norikzhagui@gmail.com>
2023-10-09 17:50:54 -0400

    button logic

--- src/components/Carousel/Card/index.tsx
+++ src/components/Carousel/Card/index.tsx
@@ -1,32 +1,41 @@
-import React from 'react';
+import React, { ReactComponentElement, ReactElement } from 'react';
+import { DateTime } from 'luxon';
 import './styles/index.css';
 
 interface CardProps {
 	jobTitle: string;
+	startDate: DateTime;
+	endDate: DateTime;
+	company: string;
+	pathToImage: string;
+	description: ReactElement;
 }
 
-const Card = () => {
+const Card = ({
+	jobTitle,
+	startDate,
+	endDate,
+	company,
+	pathToImage,
+	description,
+}: CardProps) => {
 	return (
-		<div className="card">
+		<>
 			<section className="card-data">
 				<header className="card-header">
-					<h1>SDE Intern</h1>
-					<p> - August 2023 </p>
+					<h1>{jobTitle}</h1>
+					<p>
+						{' '}
+						{startDate.toLocaleString()} - {endDate.toLocaleString()}{' '}
+					</p>
 				</header>
-				<strong>Amazon</strong>
-				<p>
-					<strong>Designed a dashboard to streamline </strong>and enhance the
-					way sellers view and comprehend the performance of their coupons.
-					Additionally, I{' '}
-					<strong>created an API and conducted end-to-end integration</strong>,
-					incorporating data visualization libraries to offer sellers actionable
-					insights aimed at <strong>improving coupon utilization.</strong>
-				</p>
+				<strong>{company}</strong>
+				<p>{description}</p>
 			</section>
 			<aside className="side-section">
-				<img src="/images/amazon-orange-logo.jpeg" className="company-logo" />
+				<img src={pathToImage} className="company-logo" />
 			</aside>
-		</div>
+		</>
 	);
 };
 
--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,18 +1,83 @@
-import React from 'react';
-import './styles/index.css';
+import React, { useRef } from 'react';
 import Card from './Card';
+import { DateTime } from 'luxon';
+import { onShiftCard } from './CarouselHelper';
+
+import './styles/index.css';
 
 const Carousel = () => {
+	const activeCard = useRef(0);
+	const containerRef = useRef<HTMLDivElement>(null);
+
 	return (
 		<>
 			<h1 className="header">Experiences</h1>
 			<div className="carousel-container">
-				<div className="carousel">
-					<Card />
-					<Card />
-					<Card />
-					<Card />
-					<Card />
+				<div className="carousel" ref={containerRef}>
+					<div className="card">
+						<Card
+							jobTitle="SDE Intern"
+							startDate={DateTime.local(2023, 6, 5)}
+							endDate={DateTime.local(2023, 8, 25)}
+							company="Amazon"
+							pathToImage="images/amazon-orange-logo.jpeg"
+							description={
+								<>
+									<strong>Designed a dashboard to streamline </strong>and
+									enhance the way sellers view and comprehend the performance of
+									their coupons. Additionally, I{' '}
+									<strong>
+										created an API and conducted end-to-end integration
+									</strong>
+									, incorporating data visualization libraries to offer sellers
+									actionable insights aimed at{' '}
+									<strong>improving coupon utilization.</strong>
+								</>
+							}
+						/>
+					</div>
+					<div className="card inactive">
+						<Card
+							jobTitle="Full Stack Web Dev"
+							startDate={DateTime.local(2022, 8, 10)}
+							endDate={DateTime.local(2023, 5, 15)}
+							company="CUNY Tech Prep"
+							pathToImage="images/cuny_tech_prep_logo.jpeg"
+							description={
+								<>
+									Learn in-demand technologies like{' '}
+									<strong>React, Node + Express, and PostgreSQL</strong> as well
+									as industry best practices such as agile & Scrum with Trello
+									and Slack, <strong>test driven development, and CI/CD</strong>
+								</>
+							}
+						/>
+					</div>
+					<div className="card inactive">
+						<Card
+							jobTitle="Full Stack Web Dev"
+							startDate={DateTime.local(2022, 8, 10)}
+							endDate={DateTime.local(2023, 5, 15)}
+							company="CUNY Tech Prep"
+							pathToImage="images/cuny_tech_prep_logo.jpeg"
+							description={
+								<>
+									Learn in-demand technologies like{' '}
+									<strong>React, Node + Express, and PostgreSQL</strong> as well
+									as industry best practices such as agile & Scrum with Trello
+									and Slack, <strong>test driven development, and CI/CD</strong>
+								</>
+							}
+						/>
+					</div>
+				</div>
+				<div className="carousel-control">
+					<button onClick={() => onShiftCard(containerRef, activeCard, -1)}>
+						left
+					</button>
+					<button onClick={() => onShiftCard(containerRef, activeCard, 1)}>
+						right
+					</button>
 				</div>
 			</div>
 		</>
