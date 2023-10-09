f97d87cecdf1e7b5c0f5062631b89572515cf3d4
AndrewGa0 <andrew.gao51@qmail.cuny.edu>
2023-10-09 19:24:24 -0400

    andrew changed his about me section

--- src/pages/DevTeamOne.tsx
+++ src/pages/DevTeamOne.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const DevTeamOne = () => {
-	return <p>Norik and Andrew Implement here</p>;
+	return <p>i fucked up badly - Andrew Implement here</p>;
 };
 
 export default DevTeamOne;
