03d02b47588343069290cdea736ae53740bbd5d3
Ovicron <Ovicrondw@gmail.com>
2023-10-08 23:27:51 -0400

    added files/directories for manually adding shadcn components and an example on the home page'

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Button } from 'src/components/ui/button'
 
 const HomePage = () => {
-	return <p>This will be the landing page for our webapp</p>
+	return (
+		<div>
+			<p>This will be the landing page for our webapp</p>
+			<Button variant="outline">EXAMPLE BUTTON</Button>
+		</div>
+	)
 }
 
 export default HomePage
