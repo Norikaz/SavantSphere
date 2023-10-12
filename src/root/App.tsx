630aa32569ce47dcd8881004b1b1cd57a7b63176
Norikaz <norikzhagui@gmail.com>
2023-10-12 13:30:11 -0400

    implementing form pagination

--- src/root/App.tsx
+++ src/root/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { ROUTES } from './routes-constants'
-import HomePage from '../pages/HomePage'
-import DevTeamOne from '../pages/AndrewNorik/DevTeamOne'
-import DevTeamTwo from '../pages/TasneemSamiul/DevTeamTwo'
-import ContactPage from '../pages/ContactPage'
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ROUTES } from './routes-constants';
+import HomePage from '../pages/HomePage';
+import DevTeamOne from '../pages/AndrewNorik/DevTeamOne';
+import DevTeamTwo from '../pages/TasneemSamiul/DevTeamTwo';
+import ContactPage from '../pages/ContactPage';
+import BestMatchPage from '../pages/BestMatchPage/';
 
 const App = () => {
 	return (
@@ -16,9 +17,10 @@ const App = () => {
 				<Route path={ROUTES.DEV_TEAM_ONE} element={<DevTeamOne />} />
 				<Route path={ROUTES.DEV_TEAM_TWO} element={<DevTeamTwo />} />
 				<Route path={ROUTES.CONTACT_PAGE} element={<ContactPage />} />
+				<Route path={ROUTES.BEST_MATCH_PAGE} element={<BestMatchPage />} />
 			</Routes>
 		</Router>
-	)
-}
+	);
+};
 
-export default App
+export default App;
