9ede14ec84e935052c86ec64991f078733eb14f3
Norikaz <norikzhagui@gmail.com>
2023-10-14 13:49:31 -0400

    pagination complete

--- src/components/AboutMeAll/AboutMeSamTas/index.tsx
+++ src/components/AboutMeAll/AboutMeSamTas/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import './styles/index.css'
-import { Avatar, AvatarFallback, AvatarImage } from 'src/components/ui/avatar'
-import SysAdminDialog from './SysAdminDialog'
-import ProjectsDialog from './ProjectsDialog'
-import { Card } from './animatedCard'
+import React from 'react';
+import './styles/index.css';
+import { Avatar, AvatarFallback, AvatarImage } from 'src/components/ui/avatar';
+import SysAdminDialog from './SysAdminDialog';
+import ProjectsDialog from './ProjectsDialog';
+import { Card } from './animatedCard';
 
 const AboutMeSam = () => {
 	return (
@@ -14,12 +14,13 @@ const AboutMeSam = () => {
 				<div>
 					<h1>About me</h1>
 					{/* className="description-sam" uses a flip animation on page load */}
-					<p className="description-sam">
-						Hello there! I am <strong>Samiul</strong>, a student and an aspiring <strong>software developer</strong>. I have experience in full stack
+					<p className="sam-description">
+						Hello there! I am <strong>Samiul</strong>, a student and an aspiring{' '}
+						<strong>software developer</strong>. I have experience in full stack
 						web development using various different frameworks and libraries.
 					</p>
 					<h1>Experiences</h1>
-					<p className="description-sam">
+					<p className="sam-description">
 						- System Administrator (March 2020 - August 2021){' '}
 						<span className="bounce">
 							<SysAdminDialog />
@@ -30,13 +31,21 @@ const AboutMeSam = () => {
 						</span>
 					</p>
 					<h1>Skills</h1>
-					<p className="description-sam">Languages: Python, C++, C, Golang, Javascript, Typescript, HTML/CSS, C#.NET, SQL</p>
-					<p className="description-sam">Frameworks: React, Next.js, Express.js, Django, Flask, FastAPI, TensorflowL</p>
-					<p className="description-sam">Tools: Linux, Git, Github, AWS, GCP, Docker, Firebase</p>
+					<p className="sam-description">
+						Languages: Python, C++, C, Golang, Javascript, Typescript, HTML/CSS,
+						C#.NET, SQL
+					</p>
+					<p className="sam-description">
+						Frameworks: React, Next.js, Express.js, Django, Flask, FastAPI,
+						TensorflowL
+					</p>
+					<p className="sam-description">
+						Tools: Linux, Git, Github, AWS, GCP, Docker, Firebase
+					</p>
 				</div>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default AboutMeSam
+export default AboutMeSam;
