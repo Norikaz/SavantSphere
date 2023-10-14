09fdf1b34e796f95421eea14e7016a2ea8f7e883
AndrewGa0 <andrew.gao51@qmail.cuny.edu>
2023-10-13 22:52:16 -0400

    made changes to AboutMeAG to make parallax effect

--- src/components/AboutMeAG/index.tsx
+++ src/components/AboutMeAG/index.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
+import 'src/components/AboutMeAG/styles/index.css';
 
-import './styles/index.css';
-
-const AboutMeAG = () => {
+const AboutMeAG: React.FC = () => {
 	return (
-		<>
-			<header className="drewheader">
-				<img src="/images/REALBACKGROUD.png" className="background" />
-				<img src="/images/layer1.png" className="foreground" />
+		<div className="parallax-container">
+			<div className="background"></div>
+			<div className="foreground"></div>
+			<div className="drewheader">
 				<a href="#" className="icon">
 					<img
-						src="https://i.pinimg.com/736x/10/ea/11/10ea11b776c5668d8d42d7dcbdb949bd.jpg"
+						src="https://media.licdn.com/dms/image/C4E03AQGWI8RvBh3l9g/profile-displayphoto-shrink_200_200/0/1566226126170?e=1702512000&v=beta&t=8xNoNJY3kp7ZkdeZrxc3kzu5qX1scKYCB64Yn0Bu5b8"
 						alt="Description of Image"
 					/>
 				</a>
 				<p className="aboutMe">
-					&quot;This is where my about me is going to go&quot;
+					I am currently studying at Queens College.
+					<br />I previously interned at PYE Education.
 				</p>
-			</header>
-		</>
+			</div>
+		</div>
 	);
 };
 
