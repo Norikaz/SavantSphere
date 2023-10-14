cc9ade50539c6bc22e588e538dbf0142a6b6abd4
Norikaz <norikzhagui@gmail.com>
2023-10-14 13:49:31 -0400

    pagination complete

---
+++ src/components/AboutMeAll/AGspacer/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles/index.css';
+
+const AGspacer: React.FC = () => {
+	return <div className="spacer"></div>;
+};
+
+export default AGspacer;
--- src/pages/AndrewNorik/DevTeamOne.tsx
+++ src/pages/AndrewNorik/DevTeamOne.tsx
@@ -5,6 +5,7 @@ import Carousel from '../../components/ui/Carousel';
 import TechStackDialog from '../../components/ui/TechStackDialog';
 import ExperienceAG from '../../components/AboutMeAll/ExperienceAG';
 import TechStackAG from '../../components/AboutMeAll/TechStackAG';
+import AGspacer from '../../components/AboutMeAll/AGspacer';
 
 import './styles/index.css';
 
@@ -15,6 +16,7 @@ const DevTeamOne = () => (
 			<Carousel />
 			<TechStackDialog />
 		</div>
+		<AGspacer />
 		<div className="wrapper">
 			<AboutMeAG />
 		</div>
