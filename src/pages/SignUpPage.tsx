1e8d0622ad898c3ae4ff34d657fce5be169caab6
AndrewGa0 <andrew.gao51@qmail.cuny.edu>
2023-10-14 17:45:11 -0400

    Added sign up form to the website

---
+++ src/pages/SignUpPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignUpForm from '../components/Forms/SignUpForm/index';
+
+const SignUpPage = () => {
+	return (
+		<div>
+			<SignUpForm />
+		</div>
+	);
+};
+
+export default SignUpPage;
